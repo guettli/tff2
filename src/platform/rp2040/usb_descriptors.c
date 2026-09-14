3dae89808540213c684b63f478ea2da67304703c
guettlibot <guettlibot@thomas-guettler.de>
2026-09-14 14:46:47 +0000

    feat(rp2040): add TinyUSB device descriptors and HID callbacks # Committing as: guettlibot@thomas-guettler.de

---
+++ src/platform/rp2040/usb_descriptors.c
@@ -0,0 +1,124 @@
+#ifdef PICO_BUILD
+
+#include "tusb.h"
+#include "pico/unique_id.h"
+#include <string.h>
+
+#define USB_PID   0x4004
+#define USB_VID   0xCafe
+#define USB_BCD   0x0200
+
+// Device Descriptor
+static tusb_desc_device_t const desc_device = {
+    .bLength            = sizeof(tusb_desc_device_t),
+    .bDescriptorType    = TUSB_DESC_DEVICE,
+    .bcdUSB             = USB_BCD,
+    .bDeviceClass       = 0x00,
+    .bDeviceSubClass    = 0x00,
+    .bDeviceProtocol    = 0x00,
+    .bMaxPacketSize0    = CFG_TUD_ENDPOINT0_SIZE,
+    .idVendor           = USB_VID,
+    .idProduct          = USB_PID,
+    .bcdDevice          = 0x0100,
+    .iManufacturer      = 0x01,
+    .iProduct           = 0x02,
+    .iSerialNumber      = 0x03,
+    .bNumConfigurations = 0x01
+};
+
+uint8_t const * tud_descriptor_device_cb(void) {
+    return (uint8_t const *) &desc_device;
+}
+
+// HID Report Descriptor: Standard keyboard report descriptor
+static uint8_t const desc_hid_report[] = {
+    TUD_HID_REPORT_DESC_KEYBOARD()
+};
+
+uint8_t const * tud_hid_descriptor_report_cb(uint8_t instance) {
+    (void) instance;
+    return desc_hid_report;
+}
+
+// Configuration Descriptor
+#define CONFIG_TOTAL_LEN  (TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN)
+#define EPNUM_HID   0x81
+
+static uint8_t const desc_configuration[] = {
+    // Config number, interface count, string index, total length, attribute, power in mA
+    TUD_CONFIG_DESCRIPTOR(1, 1, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
+    // Interface number, string index, protocol, report descriptor len, EP In address, size & polling interval
+    TUD_HID_DESCRIPTOR(0, 0, HID_ITF_PROTOCOL_KEYBOARD, sizeof(desc_hid_report), EPNUM_HID, CFG_TUD_HID_EP_BUFSIZE, 10)
+};
+
+uint8_t const * tud_descriptor_configuration_cb(uint8_t index) {
+    (void) index;
+    return desc_configuration;
+}
+
+// String Descriptors
+static char serial_str[2 * PICO_UNIQUE_BOARD_ID_SIZE_BYTES + 1] = {0};
+
+static char const *string_desc_arr[] = {
+    (const char[]) { 0x09, 0x04 }, // 0: English (0x0409)
+    "Ten Flying Fingers",          // 1: Manufacturer
+    "TFF RP2040 Keyboard",         // 2: Product
+    serial_str,                    // 3: Serial number
+};
+
+static uint16_t _desc_str[33];
+
+uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
+    (void) langid;
+    size_t chr_count = 0;
+
+    if (serial_str[0] == 0) {
+        pico_get_unique_board_id_string(serial_str, sizeof(serial_str));
+    }
+
+    if (index == 0) {
+        memcpy(&_desc_str[1], string_desc_arr[0], 2);
+        chr_count = 1;
+    } else {
+        if (index >= sizeof(string_desc_arr) / sizeof(string_desc_arr[0])) {
+            return NULL;
+        }
+
+        const char *str = string_desc_arr[index];
+        chr_count = strlen(str);
+        if (chr_count > 32) {
+            chr_count = 32;
+        }
+
+        for (size_t i = 0; i < chr_count; i++) {
+            _desc_str[1 + i] = (uint16_t)str[i];
+        }
+    }
+
+    _desc_str[0] = (uint16_t)((TUSB_DESC_STRING << 8) | (2 * chr_count + 2));
+    return _desc_str;
+}
+
+// HID Callbacks
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen) {
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) reqlen;
+    return 0;
+}
+
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
+    (void) instance;
+    (void) report_id;
+    (void) report_type;
+    (void) buffer;
+    (void) bufsize;
+}
+
+#else
+
+typedef int tff_usb_descriptors_unused_guard;
+
+#endif // PICO_BUILD
